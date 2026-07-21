9f46ac51e7370bb1dea8e59c15fe45ebab33426a
Yash Avinash Mirge <yashavinashmirge@gmail.com>
2026-07-21 23:31:15 +0530

    O2_COMPONENTS

--- MODULE02/app/index.tsx
+++ MODULE02/app/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { Image, View } from "react-native";
+import {
+  Button,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
 
 const Index = () => {
   return (
-    <View>
+    <ScrollView>
       {/* <Text>Jay Ganesh..</Text>
 
       <View>
@@ -14,8 +22,70 @@ const Index = () => {
         source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
         style={{ width: 100, height: 100, paddingTop: 24, paddingBottom: 25 }}
       />
-    </View>
+      <TextInput
+        placeholder="enter your name-:"
+        style={{ borderWidth: 1, padding: 10 }}
+      />
+      <Button
+        onPress={() => alert("Button Pressed....!")}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Pressable onPress={() => alert("Button Pressed....!")}>
+        <Text style={styles.Text}>Im pressable!</Text>
+      </Pressable>
+      {/* <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text>
+      <Text>item1</Text> */}
+      <view style={styles.container}>
+        <Text style={styles.Text}>Im pressable!</Text>
+      </view>
+    </ScrollView>
   );
 };
 
 export default Index;
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  Text: {
+    fontWeight: 800,
+    fontSize: 18,
+    color: "blue",
+  },
+});
